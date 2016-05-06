bd5500f8dd3112f5fc6ef23c7dc691ec282bb090
Scott Stevenson <scottstevenson1998@yahoo.com.au>
2016-05-06 11:29:15 +1000

    Added token expiry
    Check for token expiry and automatically request a new token if required

--- src/main/java/com/triples/corelogic/AccessToken.java
+++ src/main/java/com/triples/corelogic/AccessToken.java
@@ -1,23 +1,70 @@
 package com.triples.corelogic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.triples.corelogic.api.TokenResponse;
+import com.triples.corelogic.api.services.AccessService;
 
 @Component
 public class AccessToken {
+	private static final Logger log = LoggerFactory.getLogger(AccessToken.class);
+	private final AccessService accessService;
 	private TokenResponse token;
 	
-	public boolean bTokenReady() {
-		return (token != null);
+	@Autowired
+	public AccessToken(AccessService accessService) {
+		this.accessService = accessService;
 	}
 	
-	public void setTokenResponse(TokenResponse token) {
-		this.token = token;
+	private TokenResponse getTokenResponse() {
+		boolean bValidToken = false;
+		
+		if (token != null) {
+			if (token.isValid()){
+				bValidToken = true;
+			}
+			else {
+				log.info("Token is invalid (probably expired). Old token: {}", token);
+			}
+		}
+		
+		if (!bValidToken) {
+			log.info("Getting new token...");
+			token = accessService.getToken();
+			
+			if (token != null) {
+				if (token.isValid()){
+					log.info("New token is valid. New token: {}", token);
+					bValidToken = true;
+				}
+				else {
+					log.info("New token is invalid! New token: {}", token);
+				}
+			}
+			else {
+				log.error("Failed to get new token");
+			}
+		}
+		
+		if (bValidToken) {
+			return token;
+		}
+		else {
+			return null;
+		}
 	}
 	
 	@Override
 	public String toString() {
-		return token.getAccess_token();
+		TokenResponse response = getTokenResponse();
+		if (response != null) {
+			return response.getAccess_token();
+		}
+		else {
+			return "No valid access token";
+		}
 	}
 }
--- src/main/java/com/triples/corelogic/api/TokenResponse.java
+++ src/main/java/com/triples/corelogic/api/TokenResponse.java
@@ -1,62 +1,111 @@
 package com.triples.corelogic.api;
 
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TokenResponse {
+	private static final Logger log = LoggerFactory.getLogger(TokenResponse.class);
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private String access_token;
 	private String token_type;
 	private int expires_in;
 	private String scope;
 	private String iss;
 	private boolean env_access_restrict;
-	
+	private GregorianCalendar expiresAt;
+
 	public String getAccess_token() {
 		return access_token;
 	}
+
 	public void setAccess_token(String access_token) {
 		this.access_token = access_token;
 	}
+
 	public String getToken_type() {
 		return token_type;
 	}
+
 	public void setToken_type(String token_type) {
 		this.token_type = token_type;
 	}
+
 	public int getExpires_in() {
 		return expires_in;
 	}
+
 	public void setExpires_in(int expires_in) {
 		this.expires_in = expires_in;
+
+		expiresAt = new GregorianCalendar();
+		if (log.isDebugEnabled()) {
+			log.debug("Now:        {}", dateFormat.format(expiresAt.getTime()));
+			log.debug("Adding {} seconds for expiry", expires_in);
+		}
+
+		// Add the number of seconds until the token expires,
+		// but lets assume it expires 30 seconds earlier so we can get a new
+		// token before the old one expires
+		expiresAt.add(GregorianCalendar.SECOND, expires_in - 30);
+
+		if (log.isDebugEnabled()) {
+			log.debug("Expires At: {}", dateFormat.format(expiresAt.getTime()));
+		}
 	}
+
+	public boolean isValid() {
+		GregorianCalendar now = new GregorianCalendar();
+
+		if (log.isDebugEnabled()) {
+			log.debug("Now:        {} ({})", dateFormat.format(now.getTime()), now.getTimeInMillis());
+			log.debug("Expires At: {} ({})", dateFormat.format(expiresAt.getTime()), expiresAt.getTimeInMillis());
+			if (expiresAt.compareTo(now) > 0) {
+				log.debug("Not expired");
+			} else {
+				log.debug("Expired");
+			}
+		}
+		
+		return (expiresAt.compareTo(now) > 0);
+	}
+
 	public String getScope() {
 		return scope;
 	}
+
 	public void setScope(String scope) {
 		this.scope = scope;
 	}
+
 	public String getIss() {
 		return iss;
 	}
+
 	public void setIss(String iss) {
 		this.iss = iss;
 	}
+
 	public boolean isEnv_access_restrict() {
 		return env_access_restrict;
 	}
+
 	public void setEnv_access_restrict(boolean env_access_restrict) {
 		this.env_access_restrict = env_access_restrict;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("access_token: \"").append(access_token).append("\"")
-		  .append(", token_type: \"").append(token_type).append("\"")
-		  .append(", expires_in: ").append(expires_in)
-		  .append(", scope: \"").append(scope).append("\"")
-		  .append(", iss: \"").append(iss).append("\"")
-		  .append(", env_access_restrict: ").append(env_access_restrict);
+		sb.append("access_token: \"").append(access_token).append("\"").append(", token_type: \"").append(token_type)
+				.append("\"").append(", expires_in: ").append(expires_in).append(", scope: \"").append(scope)
+				.append("\"").append(", iss: \"").append(iss).append("\"").append(", env_access_restrict: ")
+				.append(env_access_restrict).append(", Expires at: ").append(dateFormat.format(expiresAt.getTime()));
 		return sb.toString();
 	}
 }
